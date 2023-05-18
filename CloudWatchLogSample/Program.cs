5113048aa24417a3a45058b0dddf706c37f15f5f
Daimler <daimlerv@hotmail.com>
2023-05-17 19:28:06 -0600

    first commit

---
+++ CloudWatchLogSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using CloudWatchLogSample;
+
+//This sample was taken from Ivan Kahl's Blog -> For more detail check the following: https://blog.ivankahl.com/logging-dotnet-to-aws-cloudwatch-using-awssdk/
+
+Console.WriteLine("Starting logger with CloudWatch!!");
+
+var logger = await CloudWatchLogger.GetLoggerAsync("/dev/cmaster/automatic_service");
+
+var infoWarning = "System.Diagnostics.DiagnosticSource.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.";
+
+await logger.LogMessageAsync(infoWarning);
+
+Console.WriteLine("Finised logger with CloudWatch!!");
+
+Console.ReadLine();
